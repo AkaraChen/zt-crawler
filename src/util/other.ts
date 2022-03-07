b109455ea1e34cd97891f869d58fd5f83091acff
AkaraChen <akarachen@outlook.com>
2022-03-07 21:38:37 +0800

    update

--- src/util/other.ts
+++ src/util/other.ts
@@ -1,4 +1,5 @@
 import { config } from "../config";
+import { list } from "./cover";
 
 function removeSlash(link: string) {
   if (link.endsWith("/")) return link.slice(0, link.length - 1);
@@ -11,12 +12,12 @@ function getImage(content: string): string {
     let srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
     let arr = content.match(imgReg);
     if (!arr) {
-      return config.cunstomImage;
+      return list[Math.floor(Math.random() * list.length)];
     }
     let src = arr[0].match(srcReg);
     return src[0].slice(5, src[0].length - 1);
   } catch (error) {
-    return config.cunstomImage;
+    return list[Math.floor(Math.random() * list.length)];
   }
 }
 
